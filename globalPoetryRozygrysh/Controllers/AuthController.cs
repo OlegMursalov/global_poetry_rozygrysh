eff67edf6ce51a1a93182850f410377817a3a18a
Oleg Mursalov <olofovich@mail.ru>
2021-02-22 14:13:02 +0300

    авторизация

--- globalPoetryRozygrysh/Controllers/AuthController.cs
+++ globalPoetryRozygrysh/Controllers/AuthController.cs
@@ -1,19 +1,30 @@
-﻿using System.Web.Mvc;
+﻿using globalPoetryRozygrysh.Models;
+using System.Web.Mvc;
 
 namespace globalPoetryRozygrysh.Controllers
 {
     public class AuthController : Controller
     {
         [HttpGet]
-        public ActionResult Index()
+        public ActionResult Index(AuthPerson authPerson = null)
         {
-            return View();
+            return View(authPerson);
         }
 
         [HttpPost]
         public ActionResult Process(string vk_id, string pass, string description)
         {
-            return View();
+            var authPerson = new AuthPerson
+            {
+                VK_ID = vk_id,
+                Pass = pass,
+                Description = description
+            };
+            if (!authPerson.Validate())
+            {
+                return View("Index", authPerson);
+            }
+            else return View();
         }
     }
 }
---
+++ globalPoetryRozygrysh/Models/AuthPerson.cs
@@ -0,0 +1,29 @@
+﻿namespace globalPoetryRozygrysh.Models
+{
+    public class AuthPerson
+    {
+        public string VK_ID { get; set; }
+        public string Pass { get; set; }
+        public string Description { get; set; }
+        public string TooltipText { get; set; }
+
+        public bool Validate()
+        {
+            if (string.IsNullOrEmpty(VK_ID))
+            {
+                TooltipText = "Заполните, пожалуйста, VK ID";
+                return false;
+            }
+            else if (string.IsNullOrEmpty(Pass))
+            {
+                TooltipText = "Заполните, пожалуйста, пароль";
+                return false;
+            }
+            else
+            {
+                TooltipText = null;
+                return true;
+            }
+        }
+    }
+}
