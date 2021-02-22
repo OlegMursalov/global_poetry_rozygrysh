184c508bdc27e5ac4555518658ad72abea29a803
Oleg Mursalov <olofovich@mail.ru>
2021-02-22 13:54:07 +0300

    first commit

--- globalPoetryRozygrysh/Controllers/AuthController.cs
+++ globalPoetryRozygrysh/Controllers/AuthController.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace globalPoetryRozygrysh.Controllers
 {
     public class AuthController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult Process(string vk_id, string pass, string description)
+        {
             return View();
         }
     }
