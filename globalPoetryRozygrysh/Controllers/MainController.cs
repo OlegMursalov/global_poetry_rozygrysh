10b47d4ae3610e8940544c7a7a36ed37c13b5963
Oleg Mursalov <olofovich@mail.ru>
2021-05-29 11:34:35 +0300

    УРА! GUESS RAP-LINE RZGR

---
+++ globalPoetryRozygrysh/Controllers/MainController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace globalPoetryRozygrysh.Controllers
+{
+    public class MainController : Controller
+    {
+        public MainController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
