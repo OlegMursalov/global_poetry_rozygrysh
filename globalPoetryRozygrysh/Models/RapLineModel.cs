8a7b70743d2dafb5274f1aa3c0e7b8bd152fce25
Oleg Mursalov <olofovich@mail.ru>
2021-05-29 12:47:45 +0300

    уже вырисовывается сайт (надо на мобилах провить)

--- globalPoetryRozygrysh/Models/RapLineModel.cs
+++ globalPoetryRozygrysh/Models/RapLineModel.cs
@@ -19,6 +19,12 @@ namespace globalPoetryRozygrysh.Models
             new Varik { Value = "Мой геморрой с волейбольный мяч", IsTrue = false },
             new Varik { Value = "За окном тьма и псин вой..", IsTrue = false },
             new Varik { Value = "Толстая йа трансуха-фемка, снимать трусы мне некому..", IsTrue = true },
+            new Varik { Value = "На вписке йа - банши/королева!", IsTrue = true },
+            new Varik { Value = "Мы эпохально стелим, а вы заправляете..", IsTrue = false },
+            new Varik { Value = "Братик, они трутся стручками, у них фасолины", IsTrue = false },
+            new Varik { Value = "Ха-ха-ха-ха-ха-ха-ха, нет! Не верю в это!!", IsTrue = false },
+            new Varik { Value = "Завяжи свой х*й узлом, развяжи и завяжи опять", IsTrue = false },
+            new Varik { Value = "Завяжи свой х*й узлом, развяжи и завяжи опять", IsTrue = false },
         };
 
         public IEnumerable<Varik> GetAllRapLines()
